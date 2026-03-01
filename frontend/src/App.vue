b2fb50f7bf62bd3f101f5101bd8b785bf93a1215
Gabriel <62367793+Hardcastro@users.noreply.github.com>
2026-03-01 16:28:52 -0300

    feat: código gerado automaticamente

---
+++ frontend/src/App.vue
@@ -0,0 +1,9 @@
+<template>
+  <div id="app-layout" class="h-screen bg-gray-50">
+    <RouterView />
+  </div>
+</template>
+
+<script setup lang="ts">
+import { RouterView } from 'vue-router'
+</script>
